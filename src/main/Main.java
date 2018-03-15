f16dd257016dfa1b66d35dd0aa75e4a821d301f4
andradaaag <gaeandrada@gmail.com>
2018-03-15 21:32:41 +0200

    Changed file paths

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,8 +9,8 @@ import main.ui.Console;
 public class Main {
 
     public static void main(String[] args) {
-        CharacterJSONRepository characterJSONRepository = new CharacterJSONRepository("data/characters.json");
-        PlanetJSONRepository planetJSONRepository = new PlanetJSONRepository("data/planets.json");
+        CharacterJSONRepository characterJSONRepository = new CharacterJSONRepository("./data/characters.json");
+        PlanetJSONRepository planetJSONRepository = new PlanetJSONRepository("./data/planets.json");
         ShipJSONRepository shipJSONRepository = new ShipJSONRepository("./data/ships.json");
 
         Controller ctrl = new Controller(characterJSONRepository, planetJSONRepository, shipJSONRepository);
